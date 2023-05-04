09968a2133e41be69f179bc6f04d8a2885218e19
Your Name <you@example.com>
2023-05-05 01:41:16 +0200

    ...

---
+++ mike-lauterbach-ev-differentialdiagnose/mike-02-Dateien/BUzNnIDbaDl.js
@@ -0,0 +1,4 @@
+;/*FB_PKG_DELIM*/
+
+__d("FbShortsUnifiedTofuVideoClickFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("1894");b=d("FalcoLoggerInternal").create("fb_shorts_unified_tofu_video_click",a);e=b;g["default"]=e}),98);
+__d("FbShortsUnifiedTofuVideoVpvFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("1895");b=d("FalcoLoggerInternal").create("fb_shorts_unified_tofu_video_vpv",a);e=b;g["default"]=e}),98);
